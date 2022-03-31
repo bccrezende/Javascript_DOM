1ce79d0b3d7d6a6c934b259624a6ae11542afe8b
Barbara Rezende <bccrezende@gmail.com>
2022-03-31 09:29:17 -0300

    Mudança de JS - Parte 1

--- lista.js
+++ lista.js
@@ -1,25 +1,47 @@
-// console.log('teste');
+function Submit(){
+    const valor = document.querySelector('[data-form-input]')
+    console.log(valor.value)
+
+    const novo_item_lista = document.createElement("li")
+    
+    
+    const lista = document.querySelector('[data-task]')
+
+    lista.appendChild (novo_item_lista)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function myFunction(){
 //     console.log("teste inicial");
 // }
 
-function addItem(){
-    const inputItem = document.querySelector('[data-form-input]')
-    const valorItem = inputItem.value
+// function addItem(){
+//     const inputItem = document.querySelector('[data-form-input]')
+//     const valorItem = inputItem.value
 
-    const listaDeItems = document.querySelector('[data-task]')
+//     const listaDeItems = document.querySelector('[data-task]')
 
-    novaLabel = document.createElement('label')
-    novaLabel.innerText = valorItem
+//     novaLabel = document.createElement('label')
+//     novaLabel.innerText = valorItem
     
-    novoItem = document.createElement('li')
+//     novoItem = document.createElement('li')
 
-    // novoItem.appendChild(criarBotaoConcluir())
-    novoItem.appendChild(novaLabel)
-    // novoItem.appendChild(criarBotaoDelete())
+//     // novoItem.appendChild(criarBotaoConcluir())
+//     novoItem.appendChild(novaLabel)
+//     // novoItem.appendChild(criarBotaoDelete())
 
-    listaDeItems.appendChild(novoItem)
+//     listaDeItems.appendChild(novoItem)
 
-    inputItem.value = ""
-}
+//     inputItem.value = ""
+//}
